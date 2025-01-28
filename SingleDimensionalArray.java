28c6429c2846b0f0d3e1621fd5dfdf79b62d87d6
lakshmi-192 <koyya.lakshmi12@gmail.com>
2025-01-28 20:19:00 +0530

    Add files via upload
    Single dimensional array

---
+++ SingleDimensionalArray.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class SingleDimensionalArray {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the size of the array: ");
+    int size = sc.nextInt();
+    int[] array = new int[size];
+    System.out.println("Enter " + size + " elements:");
+    for (int i = 0; i < size; i++) {
+      array[i] = sc.nextInt();
+    }
+    System.out.println("Array elements:");
+    for (int i = 0; i < size; i++) {
+      System.out.print(array[i] + " ");
+    }
+  }
+}
